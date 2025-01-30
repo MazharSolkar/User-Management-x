b29f0722acb2bc8d8793b6305dc5828a197f76a3
MazharSolkar <mazharsolkar65@gmail.com>
2025-01-31 05:26:36 +0530

    Add test page and implement authentication middleware

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,13 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
-<body>
 
+<body>
     <x-navbar />
+    <x-message />
+    <h1 class="fw-bold text-center my-4">{{$title}}</h1>
+
+    <div>{{$main}}</div>
 </body>
+
 </html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>Test Page</x-slot>
+    <x-slot:main>
+        <div>
+            <h1>this is main content</h1>
+        </div>
+    </x-slot>
+</x-layout>
